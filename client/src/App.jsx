e2aec9b1d5a35883426e3e60dd8cca04b1a92b3b
Anand Chauhan <107473922+ana4nd@users.noreply.github.com>
2025-10-08 14:29:50 +0530

    fix background

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   const navigate = useNavigate();
 
   return (
-   <div className="bg-[url('/src/assets/bgImage.svg')] bg-contain">
+   <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster/>
       <Routes>
         <Route path="/" element={authUser ? <HomePage/> : <Navigate to={"/login"}/> } />
